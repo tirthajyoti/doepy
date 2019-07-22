8e15ede012cff28c7f21ff455a0f7ed21144f5fd
Tirthajyoti Sarkar <tirthajyoti@gmail.com>
2019-07-21 18:41:02 -0700

    Added some import statements

--- doepy/__init__.py
+++ doepy/__init__.py
@@ -1,1 +1,5 @@
-pass
+from pyDOE import *
+from doepy.pydoe_corrected import fullfact_corrected,fracfact_corrected, fracfact_by_res,bbdesign_corrected, ccdesign_corrected
+from diversipy import *
+import pandas as pd
+import numpy as np
